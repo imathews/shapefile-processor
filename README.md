20e93927c507b6673b79af1dbb67c1ad5873a31f
Ian Mathews <imathews@users.noreply.github.com>
2020-07-05 17:41:00 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Shapefile processor
+Converts shapefiles to CSVs for upload to Redivis
+
+## Usage
+First, clone this to your computer and run 
+```
+npm install
+```
+
+Next, run:
+```
+npm start -- path_to_shp path_to_dbf [path_to_outfile.csv]
+```
+Where the first two arguments are the paths to the shapefile (.shp) and database file (.dbf), respectively.
+
+The final argument is the path to write the csv output to. If not path is provided, the csv will be written to `converted_shapefile.csv` in the current working directory.
---
+++ index.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+
+const stringify = require('csv-stringify');
+const topojson = require('topojson-server');
+const { simplify, presimplify } = require('topojson-simplify');
+const { feature } = require('topojson-client');
+const shapefile = require('shapefile');
+const geobuf = require('geobuf');
+const Pbf = require('pbf');
+
+const shpPath = process.argv[2];
+const dbfPath = process.argv[3];
+const outfilePath = process.argv[4] || 'converted_shapefile.csv';
+
+const simplificationLevels = [1, 10, 20, 50];
+
+shapefile
+	.read(shpPath, dbfPath)
+	.then((res) => {
+		const properties = getAllProperties(res.features);
+		let topology = topojson.topology([res], 1e5);
+
+		topology = presimplify(topology);
+		const arcWeightPercentiles = getArcWeightPercentiles(topology.arcs);
+		const simplifiedGeoJson = simplificationLevels.map((level, i) =>
+			feature(simplify(topology, arcWeightPercentiles[i]), topology.objects[Object.keys(topology.objects)[0]]),
+		);
+
+		const csvData = [];
+		for (let i = 0; i < res.features.length; i++) {
+			const feature = res.features[i];
+			const row = {};
+			for (const property of properties) {
+				row[property] = feature.properties[property];
+			}
+			feature.properties = undefined;
+			row.geojson = JSON.stringify(feature);
+			row.geobuf = Buffer.from(geobuf.encode(feature, new Pbf())).toString('base64');
+			for (let n = 0; n < simplificationLevels.length; n++) {
+				const feature = simplifiedGeoJson[n].features[i];
+				feature.properties = undefined;
+				row[`geobuf_simplified_${simplificationLevels[n]}`] = Buffer.from(
+					geobuf.encode(feature, new Pbf()),
+				).toString('base64');
+			}
+			csvData.push(row);
+		}
+		stringify(csvData, { header: true }).pipe(fs.createWriteStream(outfilePath));
+	})
+	.catch(console.log);
+
+// Returns an array of all properties observed across the features
+function getAllProperties(features) {
+	const propertiesSet = new Set();
+	for (const feature of features) {
+		for (const property in feature.properties) {
+			propertiesSet.add(property);
+		}
+	}
+	return [...propertiesSet];
+}
+
+function getArcWeightPercentiles(arcs) {
+	const arcAreas = [];
+
+	for (const arc of arcs) {
+		for (const coord of arc) {
+			if (coord[2] !== Infinity) {
+				arcAreas.push(coord[2]);
+			}
+		}
+	}
+
+	arcAreas.sort((a, b) => a - b);
+
+	return simplificationLevels.map((level) => {
+		return arcAreas[arcAreas.length - 1 - Math.floor(arcAreas.length / (100 / level))];
+	});
+}
